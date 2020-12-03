4b54bfc0412a60dd37041bb8e5df25f2c959c672
Ayushi <aprakash162001@gmail.com>
2020-12-04 00:26:31 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [joke, setJoke] = useState("Loading...");
+  const [timeRemaining, setTimeRemaining] = useState(800);
+
+  useEffect(() => {
+    setInterval(() => {
+      axios
+        .get("https://icanhazdadjoke.com/", {
+          headers: {
+            Accept: "application/json",
+          },
+        })
+        .then((response) => {
+          setJoke(response.data.joke);
+          setTimeRemaining(800);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 8000);
+
+    setInterval(() => {
+      setTimeRemaining((time) => {
+        if (time <= 0) {
+          return 800;
+        } else return time - 1;
+      }); //updating the state using its own current value
+    }, 10);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{joke}</div>
+      <div
+        className="progressBar"
+        style={{ width: `${((800 - timeRemaining) / 800) * 100}%` }}
+      ></div>
     </div>
   );
 }
